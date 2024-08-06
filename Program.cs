5b110fef15b2e78eaaea5c139240c853e37e626c
unknown <souryzetma1144@yahoo.com>
2024-08-06 13:57:41 +0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+
+        string yourName = Console.ReadLine();
+
+        Console.WriteLine("Hello: " + yourName);
+    }
+}
